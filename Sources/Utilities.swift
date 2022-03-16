538956407052ec290642cf6b2cf15db565e055b0
Hoang Anh Ngo <tonyngo.jr@gmail.com>
2022-03-16 11:30:13 +0100

    chore: increase epsilon

--- Sources/Utilities.swift
+++ Sources/Utilities.swift
@@ -31,7 +31,7 @@
 
 // Tolerance used for calculating approximate equality
 //let epsilon = 1e-8
-let epsilon = 1e-12
+let epsilon = 1e-6
 
 // Round-off floating point values to simplify equality checks
 func quantize(_ value: Double) -> Double {
